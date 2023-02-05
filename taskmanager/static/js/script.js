12f8e95da89b4304e6aa618030e20e256ef0ab7d
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-02-05 18:58:51 +0000

    Adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     //sidenav initilization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    // internationalization option, to help translate text when dealing with a foreign language.
+    i18n: {done: "Select"}
+    });
+    
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });
